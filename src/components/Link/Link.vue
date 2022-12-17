e0696364b90a23f092a258e2882f62c2530b9b69
tfarley <heavyt12@gmail.com>
2022-12-17 16:56:00 -0700

    built current version

---
+++ src/components/Link/Link.vue
@@ -0,0 +1,32 @@
+<template>
+	<a
+		v-bind="$attrs"
+		class="ty-link"
+		:href="href"
+		:target="target"
+		v-on="$listeners"
+	>
+		<slot />
+	</a>
+</template>
+
+<script>
+	/** Specialized link component. */
+	export default {
+		name: 'TyLink',
+
+		inheritAttrs: false,
+
+		props: {
+			href: {
+				type: String,
+				required: true
+			},
+
+			target: {
+				type: String,
+				default: '_self'
+			}
+		}
+	};
+</script>
